9587a91c0eb7ca41312ef8d6906e790c48a46f89
rysmith <ryansmith05@gmail.com>
2015-07-08 18:33:39 -0700

    generate location model

---
+++ app/models/location.server.model.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Location Schema
+ */
+var LocationSchema = new Schema({
+	// Location model fields   
+	// ...
+});
+
+mongoose.model('Location', LocationSchema);
---
+++ app/tests/location.server.model.test.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var should = require('should'),
+	mongoose = require('mongoose'),
+	User = mongoose.model('User'),
+	Location = mongoose.model('Location');
+
+/**
+ * Globals
+ */
+var user, location;
+
+/**
+ * Unit tests
+ */
+describe('Location Model Unit Tests:', function() {
+	beforeEach(function(done) {
+		user = new User({
+			firstName: 'Full',
+			lastName: 'Name',
+			displayName: 'Full Name',
+			email: 'test@test.com',
+			username: 'username',
+			password: 'password'
+		});
+
+		user.save(function() { 
+			location = new Location({
+				// Add model fields
+				// ...
+			});
+
+			done();
+		});
+	});
+
+	describe('Method Save', function() {
+		it('should be able to save without problems', function(done) {
+			return location.save(function(err) {
+				should.not.exist(err);
+				done();
+			});
+		});
+	});
+
+	afterEach(function(done) { 
+		Location.remove().exec();
+		User.remove().exec();
+
+		done();
+	});
+});
