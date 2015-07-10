21f30a08bad563f02cd361a4c54609b79b9f9991
rysmith <ryansmith05@gmail.com>
2015-07-10 11:43:17 -0700

    generate express controller, model, routes, and test for location

---
+++ app/models/location.server.model.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Location Schema
+ */
+var LocationSchema = new Schema({
+
+});
+
+mongoose.model('Location', LocationSchema);
