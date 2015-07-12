73f87b879967723dce5d06fb2de72c8e16de01a5
rysmith <ryansmith05@gmail.com>
2015-07-12 15:12:01 -0700

    add API call to ArcGIS to parse user input

--- app/controllers/locations.server.controller.js
+++ app/controllers/locations.server.controller.js
@@ -9,39 +9,77 @@ var mongoose = require('mongoose'),
     errorHandler = require('./errors.server.controller'),
     Location = mongoose.model('Location');
 
+var parseSearch = function(req, res, callback) {
+    request.post({
+        url: 'http://geoenrich.arcgis.com/arcgis/rest/services/World/geoenrichmentserver/StandardGeographyQuery/execute',
+        json:true,
+        form: {
+            f: 'pjson',
+            token: process.env.ESRI_TEMP_TOKEN,
+            sourceCountry: 'US',
+            geographyIDs: '["' + req.body.location + '"]',
+            geographylayers: '["US.ZIP5"]',
+            returnGeometry: true,
+            generalizationLevel: 5
+        }
+    }, function(error, response, body){
+        callback(body.results[0]);
+    });
+};
+
+
+// get geographic enrichment data from ArcGIS
+var getGeoData = function(req, res, callback) {
+    request.post({
+        url: 'http://geoenrich.arcgis.com/arcgis/rest/services/World/GeoenrichmentServer/Geoenrichment/enrich',
+        json:true,
+        form: {
+            f: 'json',
+            token: process.env.ESRI_TEMP_TOKEN,
+            studyAreas: '[{"address":{"text":"' + req.body.location + '"}}]'
+        }
+    }, function(error, response, body){
+        callback(body.results[0].value.FeatureSet[0].features[0]);
+    });
+}; // end of getGeoData
 exports.search = function(req, res) {
 
-    console.log(req.body);
-
-    var getGeoData = function(callback) {
-        request.post({
-            url: 'http://geoenrich.arcgis.com/arcgis/rest/services/World/GeoenrichmentServer/Geoenrichment/enrich',
-            json:true,
-            form: {
-                f: 'json',
-                token: process.env.ESRI_TEMP_TOKEN,
-                studyAreas: '[{"address":{"text":"601 E. Anapamu Street, Santa Barbara"}}]'
-            }
-        }, function(error, response, body){
-            console.log(body.results[0].value.FeatureSet[0]);
-            callback(body.results[0].value.FeatureSet[0].features[0]);
-        });
-    }; // end of getGeoData
-
-    getGeoData(function(geoResponse) {
-
-        var location = new Location(geoResponse);
-
-        location.save(function(err) {
-            if (err) {
-                return res.status(400).send({
-                    message: errorHandler.getErrorMessage(err)
-                });
-            } else {
-                return res.json(geoResponse);
-            }
-        });
+    parseSearch(req, res, function(parsedResponse) {
+        console.log(parsedResponse);
+        return res.json(parsedResponse)
     });
+
+    // once the geographic data is received check to see if it's already in the db, if not, save it
+    //getGeoData(req, res, function(geoResponse) {
+    //
+    //    var location = new Location(geoResponse);
+    //
+    //    Location.find().populate('_id', 'attributes').exec(function(err, locations) {
+    //        if (err) {
+    //            return res.status(400).send({
+    //                message: errorHandler.getErrorMessage(err)
+    //            });
+    //        } else {
+    //            for (var i in locations) {
+    //                if ( locations[i].attributes.X === location.attributes.X && locations[i].attributes.Y === location.attributes.Y ) {
+    //                    console.log('we have a match for array index ' + i + ' so we will not save');
+    //                    return res.json(geoResponse);
+    //                }
+    //            }
+    //
+    //            location.save(function(err) {
+    //                if (err) {
+    //                    return res.status(400).send({
+    //                        message: errorHandler.getErrorMessage(err)
+    //                    });
+    //                } else {
+    //                    return res.json(geoResponse);
+    //                }
+    //            });
+    //
+    //        }
+    //    }); // end of Location.find
+    //}); // end of getGeoData
 
 }; // end of search
 
