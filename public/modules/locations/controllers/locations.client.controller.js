3c4cc83bfe72c6a7d8fe316b5f3ee5499a732a32
rysmith <ryansmith05@gmail.com>
2015-07-16 10:24:58 -0700

    debug zillow response

--- public/modules/locations/controllers/locations.client.controller.js
+++ public/modules/locations/controllers/locations.client.controller.js
@@ -10,9 +10,12 @@ angular.module('locations').controller('LocationsController', ['$scope', '$locat
 			});
 			location.$save(function (response) {
 				console.log(response.yelp);
-                $scope.zillowCharts = response.zillow.charts[0].chart;
-                $scope.zillowDemographics = response.zillow.pages[0].page[2].tables[0].table[0].data[0].attribute;
-                $scope.zillowRegion = response.zillow.region[0];
+				console.log(response.zillow.charts[0].chart);
+				console.log(response.zillow.pages[0].page[2].tables[0].table[0].data[0].attribute);
+				console.log(response.zillow.region[0]);
+                //$scope.zillowCharts = response.zillow.charts[0].chart;
+                //$scope.zillowDemographics = response.zillow.pages[0].page[2].tables[0].table[0].data[0].attribute;
+                //$scope.zillowRegion = response.zillow.region[0];
 				$scope.yelp = response.yelp.businesses;
                 $scope.location = '';
                 var currentSearchLocation = new google.maps.LatLng(response.zillow.region[0].latitude[0], response.zillow.region[0].longitude[0]);
