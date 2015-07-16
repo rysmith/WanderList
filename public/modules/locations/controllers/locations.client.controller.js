94da593045520414dbbac18ef77485f5757e8e03
rysmith <ryansmith05@gmail.com>
2015-07-16 10:49:12 -0700

    debug ngmap

--- public/modules/locations/controllers/locations.client.controller.js
+++ public/modules/locations/controllers/locations.client.controller.js
@@ -10,18 +10,15 @@ angular.module('locations').controller('LocationsController', ['$scope', '$locat
 			});
 			location.$save(function (response) {
 				console.log(response.yelp);
-				//console.log(response.zillow.charts[0].chart);
-				//console.log(response.zillow.pages[0].page[2].tables[0].table[0].data[0].attribute);
-				//console.log(response.zillow.region[0]);
-                //$scope.zillowCharts = response.zillow.charts[0].chart;
-                //$scope.zillowDemographics = response.zillow.pages[0].page[2].tables[0].table[0].data[0].attribute;
-                //$scope.zillowRegion = response.zillow.region[0];
+                $scope.zillowCharts = response.zillow.charts[0].chart;
+                $scope.zillowDemographics = response.zillow.pages[0].page[2].tables[0].table[0].data[0].attribute;
+                $scope.zillowRegion = response.zillow.region[0];
 				$scope.yelp = response.yelp.businesses;
                 $scope.location = '';
-                var currentSearchLocation = new google.maps.LatLng(response.zillow.region[0].latitude[0], response.zillow.region[0].longitude[0]);
-                if (response) {
-                    $scope.map.setCenter(currentSearchLocation);
-                }
+                //var currentSearchLocation = new google.maps.LatLng(response.zillow.region[0].latitude[0], response.zillow.region[0].longitude[0]);
+                //if (response) {
+                //    $scope.map.setCenter(currentSearchLocation);
+                //}
 			}, function (errorResponse) {
 				$scope.error = errorResponse.data.message;
 			});
