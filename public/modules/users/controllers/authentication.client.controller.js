57b0ed96a4b62d9b2b9c9d3dbe90a2869a0c0363
rysmith <ryansmith05@gmail.com>
2015-07-16 16:56:21 -0700

    fix background images and redirect to /locations after signin/signup

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -12,8 +12,8 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 				// If successful we assign the response to the global user model
 				$scope.authentication.user = response;
 
-				// And redirect to the index page
-				$location.path('/');
+				// And redirect to the locations page
+				$location.path('/locations');
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
@@ -24,8 +24,8 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 				// If successful we assign the response to the global user model
 				$scope.authentication.user = response;
 
-				// And redirect to the index page
-				$location.path('/');
+				// And redirect to the locations page
+				$location.path('/locations');
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
