9d5152bc12531ce8c482ba6557593bd1c638d1d5
rysmith <ryansmith05@gmail.com>
2015-07-16 10:10:00 -0700

    use minified ngMap script for production

--- config/env/production.js
+++ config/env/production.js
@@ -18,7 +18,7 @@ module.exports = {
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-				'public/lib/ngmap/build/scripts/ng-map.js'
+				'public/lib/ngmap/build/scripts/ng-map.min.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
